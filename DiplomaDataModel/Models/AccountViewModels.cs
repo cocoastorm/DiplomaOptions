f567a46bda05a0b65b5d9aaa497ad9db9e846eac
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-02-17 13:58:51 -0800

    Add missing files and change login to use username instead of email

--- DiplomaDataModel/Models/AccountViewModels.cs
+++ DiplomaDataModel/Models/AccountViewModels.cs
@@ -6,8 +6,8 @@ namespace OptionsWebSite.Models
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
     }
 
     public class ExternalLoginListViewModel
@@ -49,9 +49,8 @@ namespace OptionsWebSite.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -64,6 +63,10 @@ namespace OptionsWebSite.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]

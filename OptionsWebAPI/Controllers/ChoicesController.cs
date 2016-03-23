b1400ed3c7fc29963c1512092d0ef98fc01a830d
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-03-23 04:07:35 +0000

    enable cors, change register api to match username instead of email

---
+++ OptionsWebAPI/Controllers/ChoicesController.cs
@@ -0,0 +1,50 @@
+﻿using DiplomaDataModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+
+namespace OptionsWebAPI.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class ChoicesController : ApiController
+    {
+        private DiplomaContext db = new DiplomaContext();
+
+        public IEnumerable<Choice> GetAllChoices()
+        {
+            return db.Choices.ToList();
+        }
+
+        public IHttpActionResult GetChoiceById(int id)
+        {
+            var choice = db.Choices.FirstOrDefault(c=>c.ChoiceId == id);
+
+            if(choice == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(choice);
+        }
+
+        [ResponseType(typeof(Choice))]
+        public IHttpActionResult PostChoice(Choice choice)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Choices.Add(choice);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = choice.ChoiceId }, choice);
+        }
+    }
+}

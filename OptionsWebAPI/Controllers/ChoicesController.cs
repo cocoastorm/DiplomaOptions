0ba34ac48e8998820674756897f762a53fc2f8ca
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-03-23 23:37:59 +0000

    change choicescontroller web api to output json to required graph data

--- OptionsWebAPI/Controllers/ChoicesController.cs
+++ OptionsWebAPI/Controllers/ChoicesController.cs
@@ -1,4 +1,5 @@
 ﻿using DiplomaDataModel.Models;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -18,9 +19,38 @@ namespace OptionsWebAPI.Controllers
     {
         private DiplomaContext db = new DiplomaContext();
 
-        public IEnumerable<Choice> GetAllChoices()
+        public JObject GetAllChoices()
         {
-            return db.Choices.ToList();
+            JObject optionsForAChoice = new JObject();
+            JObject allChoices = new JObject();
+            JObject choiceNum = new JObject();
+            int[] arr_choice;
+            var options = db.Options.Select(o => o.OptionId).ToArray();
+
+            //Choices for YearTermID 2 - 201530
+            int?[] choice1 = db.Choices.Where(c => c.YearTermId == 2).Select(c => c.FirstChoiceOptionId).ToArray();
+            int?[] choice2 = db.Choices.Where(c => c.YearTermId == 2).Select(c => c.SecondChoiceOptionId).ToArray();
+            int?[] choice3 = db.Choices.Where(c => c.YearTermId == 2).Select(c => c.ThirdChoiceOptionId).ToArray();
+            int?[] choice4 = db.Choices.Where(c => c.YearTermId == 2).Select(c => c.FourthChoiceOptionId).ToArray();
+
+            //An array that contains arrays of choices
+            int?[][] choices = new int?[][] { choice1, choice2, choice3, choice4 };
+
+            //Iterate through each choices: 1-4
+            for (int k = 0; k < choices.Length; k++)
+            {
+                optionsForAChoice = new JObject();
+                arr_choice = new int[options.Length + 1];   //Array holder for current iteration of all choices
+                for (int j = 0; j < choices[k].Length; j++) //Iterate through individual arrays of choices, 1st 2nd 3rd 4th choices
+                    arr_choice[(int)choices[k][j]]++;  //Increment any repeating choices
+                for (int i = 1; i <= options.Length; i++)
+                    optionsForAChoice.Add("C" + (k + 1) + "_" + i.ToString(), arr_choice[i]);  //Add tags to identify each choice
+
+                choiceNum.Add("Choice" + (k + 1), optionsForAChoice);
+            }
+            allChoices.Add("201530", choiceNum);
+
+            return allChoices;
         }
 
         public IHttpActionResult GetChoiceById(int id)
@@ -83,22 +113,6 @@ namespace OptionsWebAPI.Controllers
             }
 
             return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // DELETE: api/Choices/5
-        [ResponseType(typeof(Choice))]
-        public IHttpActionResult DeleteChoice(int id)
-        {
-            Choice choice = db.Choices.Find(id);
-            if (choice == null)
-            {
-                return NotFound();
-            }
-
-            db.Choices.Remove(choice);
-            db.SaveChanges();
-
-            return Ok(choice);
         }
 
         private bool ChoiceExists(int id)

9ccf286a406455f292b194bb4209032c78687b53
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-03-24 01:34:51 -0700

    add authorization token to header

--- OptionsClient/script.js
+++ OptionsClient/script.js
@@ -16,7 +16,7 @@ optionsClient.controller('mainController', function ($scope, $http, $window, $ht
                 $scope.message = "Registered!";
             })
             .error(function (data, status, headers, config) {
-                $scope.message = "Failed Registration!";
+                $scope.message = data;
             });
         };
 
@@ -36,7 +36,30 @@ optionsClient.controller('mainController', function ($scope, $http, $window, $ht
             })
             .error(function (data, status, headers, config) {
                 delete $window.sessionStorage.token;
-                $scope.message = "Failed Logged In";
+                $scope.message = data;
             });
         };
+});
+
+// append token to every request
+optionsClient.factory('authInterceptor', function ($rootScope, $q, $window) {
+    return {
+        request: function (config) {
+            config.headers = config.headers || {};
+            if ($window.sessionStorage.token) {
+                config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+            }
+            return config;
+        },
+        response: function (response) {
+            if (response.status === 401) {
+                // handle the case where the user is not authenticated
+            }
+            return response || $q.when(response);
+        }
+    };
+});
+
+optionsClient.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
 });

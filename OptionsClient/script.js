e14f37f7449010a2fe91f718252394b241b90987
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-03-25 08:18:40 +0000

    fix typo, register and other ajax requests work now

--- OptionsClient/script.js
+++ OptionsClient/script.js
@@ -7,11 +7,11 @@ optionsClient.controller('mainController', function ($scope, $http, $window, $ht
             Username : $("#r_username").val(),
             Email : $("#r_email").val(),
             Password : $("#r_password").val(),
-            ConfirmPassword : $("#r_cpassword").val()
+            ConfirmPassword: $("#r_cpassword").val()
         };
 
         $http
-            .post('http://a2api.nguyenkhoat.com/api/Account/Register', $scope.user)
+            .post('http://a2api.nguyenkhoat.com/api/Account/Register', $scope.newuser)
             .success(function (data, status, headers, config) {
                 $scope.message = "Registered!";
             })
@@ -42,7 +42,7 @@ optionsClient.controller('mainController', function ($scope, $http, $window, $ht
 
     $scope.choices = function () {
         $http
-        .get('http://a2api.nguyenkhoat.com/Choices')
+        .get('http://a2api.nguyenkhoat.com/api/Choices')
         .success(function (data, status, headers, config) {
             $scope.message = data;
         })
@@ -53,25 +53,26 @@ optionsClient.controller('mainController', function ($scope, $http, $window, $ht
 
 });
 
+// DISABLED FOR NOW! NOT IMPLEMENTED IN API!!
 // append token to every request
-optionsClient.factory('authInterceptor', function ($rootScope, $q, $window) {
-    return {
-        request: function (config) {
-            config.headers = config.headers || {};
-            if ($window.sessionStorage.token) {
-                config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
-            }
-            return config;
-        },
-        response: function (response) {
-            if (response.status === 401) {
-                // handle the case where the user is not authenticated
-            }
-            return response || $q.when(response);
-        }
-    };
-});
+//optionsClient.factory('authInterceptor', function ($rootScope, $q, $window) {
+//    return {
+//        request: function (config) {
+//            config.headers = config.headers || {};
+//            if ($window.sessionStorage.token) {
+//                config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+//            }
+//            return config;
+//        },
+//        response: function (response) {
+//            if (response.status === 401) {
+//                // handle the case where the user is not authenticated
+//            }
+//            return response || $q.when(response);
+//        }
+//    };
+//});
 
-optionsClient.config(function ($httpProvider) {
-    $httpProvider.interceptors.push('authInterceptor');
-});
+//optionsClient.config(function ($httpProvider) {
+//    $httpProvider.interceptors.push('authInterceptor');
+//});
